1d22c69b993dd1a25abb2d8815633b98e48528fa
Twig <git@twig.gay>
2026-02-27 10:30:47 -0700

    Tweaked build configuration - renamed project - disables `BUILD_SHARED_LIBS`

--- debug.sh
+++ debug.sh
@@ -2,7 +2,7 @@
 
 if [[ $1 = "-b" ]]; then
     echo rebuilding...
-    cmake --build build --target knight_pipe || exit 1
+    cmake --build build --target knight_streamer || exit 1
     echo ---
 fi
-bin/Debug/knight_pipe
+bin/Debug/knight_streamer
