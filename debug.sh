965454ef9be9137af2200c66000a9f55464556b8
Twig <git@twig.gay>
2026-03-02 11:38:18 -0700

    Added environment variables to debug script

--- debug.sh
+++ debug.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+set -e
+export $(grep -v '^#' .env | xargs)
 
 if [[ $1 = "-b" ]]; then
     echo rebuilding...
     cmake --build build --target knight_streamer || exit 1
     echo ---
 fi
-bin/Debug/knight_streamer
+bin/Debug/knight_streamer --montage $MONTAGE
